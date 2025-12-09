fb42f2a27c2d2d3879a8008711c661b79e25d700
Aubrey Baxton <bg548@protonmail.com>
2025-12-10 00:04:35 +0530

    added cookie

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { corsOptions } from './constants/config.ts';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 
 const PORT = process.env.PORT || 14000;
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
